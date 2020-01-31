877d08ac2c1f34b6915e4c6c7061c10b6b69f05f
@fvesterg <fredrik.vestergaard@zhdk.ch>
2020-01-31 22:00:22 +0100

    :bouquet:

---
+++ Repair Beavers in Space/Assets/Scritps/Spaceship.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    public static Spaceship instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+}
