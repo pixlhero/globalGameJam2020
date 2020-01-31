993328adaa8447a6c7f80ba955ca66ae7591c9c9
@fvesterg <fredrik.vestergaard@zhdk.ch>
2020-01-31 20:53:40 +0100

    :squirrel:

---
+++ Repair Beavers in Space/Assets/Scritps/Spawner/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public static Spawner instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+}
