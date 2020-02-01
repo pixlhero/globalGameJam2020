f9cae29bf02a60541ce9664614444e00cf7fb8e2
pixlhero <raphaelanderegg@hotmail.com>
2020-02-01 16:59:54 +0100

    :computer:
    ui screens

---
+++ Repair Beavers in Space/Assets/Scritps/UI/PlayerSelectScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelectScreen : UIScreen{
+    public override void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public override void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
