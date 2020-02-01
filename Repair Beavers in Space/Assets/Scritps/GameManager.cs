62fb3df2066173ec4785537562e4db383b4b5eaa
pixlhero <raphaelanderegg@hotmail.com>
2020-02-01 15:43:18 +0100

    :movie_camera:
    scene loading

--- Repair Beavers in Space/Assets/Scritps/GameManager.cs
+++ Repair Beavers in Space/Assets/Scritps/GameManager.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private bool _hasStartedFromThisScene; // used for debugging. e.g. skipping the UI introduction
+
+    private void Start()
     {
-        
+        if (SceneManager.sceneCount < 2)
+        { // to make starting from this scene possible
+            SceneManager.LoadScene("UIScene", LoadSceneMode.Additive);
+            _hasStartedFromThisScene = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetNumberOfPlayers(int number)
     {
-        
+        // TODO
+    }
+
+    public void StartGame()
+    {
+        //TODO
     }
 }
--- Repair Beavers in Space/Assets/Scritps/UIManager.cs
+++ Repair Beavers in Space/Assets/Scritps/UIManager.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    private bool _hasStartedFromMainScene; // used for debugging. e.g. skipping the UI introduction
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (SceneManager.sceneCount < 2)
+        {
+            SceneManager.LoadScene("MainScene", LoadSceneMode.Additive);
+        }
+        else
+        {
+            _hasStartedFromMainScene = true;
+        }
     }
 
     // Update is called once per frame
