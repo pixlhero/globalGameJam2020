1779c0a66569d78c6eeea1c76b5281eec3fbeb90
pixlhero <raphaelanderegg@hotmail.com>
2020-02-01 09:48:29 +0100

    :christmas_tree:

---
+++ Repair Beavers in Space/Assets/Scritps/Player/PlayerGrabbing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabbing : MonoBehaviour
+{
+
+    public Transform grabAnchor;
+
+    private Grabbable newestGrabbable;
+    private Grabbable heldObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TryGrabbing()
+    {
+        if(this.newestGrabbable != null)
+        {
+            ExecuteGrab(this.newestGrabbable);
+            this.newestGrabbable = null;
+        }
+    }
+
+    private void ExecuteGrab(Grabbable grabbable)
+    {
+        grabbable.rigidbody.isKinematic = true;
+        grabbable.transform.SetParent(grabAnchor);
+        grabbable.transform.localPosition = Vector3.zero;
+    }
+
+    // these 2 methods should be reworked later
+    public void CanGrabThis(Grabbable grabbable)
+    {
+        this.newestGrabbable = grabbable;
+    }
+
+    public void CannotGrabThisAnymore(Grabbable grabbable)
+    {
+        if(this.newestGrabbable == grabbable)
+        {
+            this.newestGrabbable = null;
+        }
+    }
+}
