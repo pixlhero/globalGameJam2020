88a480d2995a55a0efd16c2521c5368166e51e8f
pixlhero <raphaelanderegg@hotmail.com>
2020-02-02 13:30:33 +0100

    :100: :credit_card:

--- Repair Beavers in Space/Assets/Scritps/UI/CreditsScreen.cs
+++ Repair Beavers in Space/Assets/Scritps/UI/CreditsScreen.cs
@@ -3,9 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CreditsScreen : UIScreen
 {
+    public float speed = 1;
+
+    public RawImage CreditsImage;
+
+    public CanvasGroup group;
+
     public override void Hide()
     {
         SceneManager.LoadScene("UIScene");
@@ -13,7 +20,34 @@ public class CreditsScreen : UIScreen
 
     public override void Show()
     {
-        this.transform.position += new Vector3(0, -1000, 0);
-        this.transform.DOMoveY(this.transform.position.y + 2000, 10).OnComplete(() => this.Hide());
+        this.gameObject.SetActive(true);
+        group.alpha = 0;
+
+        group.DOFade(1, 0.5f);
+
+
+        Rect uv = CreditsImage.uvRect;
+        uv.y = 0.9f;
+        CreditsImage.uvRect = uv;
+
+    }
+
+    private void Update()
+    {
+        Rect uv = CreditsImage.uvRect;
+        uv.y -= Time.deltaTime * speed;
+        CreditsImage.uvRect = uv;
+
+        if (uv.y < 0.07)
+        {
+            if (Input.GetButtonDown("Start"))
+            {
+                Hide();
+            }
+            uv.y = 0.07f;
+            CreditsImage.uvRect = uv;
+
+        }
+
     }
 }
