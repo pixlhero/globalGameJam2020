0af0f755990564477da29788fe84a58d1c832520
pixlhero <raphaelanderegg@hotmail.com>
2020-02-02 12:12:10 +0100

    :milky_way: :baby_bottle:

---
+++ Repair Beavers in Space/Assets/Scritps/UI/MilkBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MilkBar : MonoBehaviour
+{
+    public float fillStand;
+
+    public float waveSpeed;
+
+    public RawImage WaveBack;
+    public RawImage WaveForward;
+    public RectTransform MilkImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rect backUV = WaveBack.uvRect;
+        backUV.x += Time.deltaTime * waveSpeed;
+        WaveBack.uvRect = backUV;
+
+
+        Rect forwardUV = WaveForward.uvRect;
+        forwardUV.x -= Time.deltaTime * waveSpeed;
+        WaveForward.uvRect = forwardUV;
+
+        MilkImage.localScale = new Vector3(1, fillStand, 1);
+    }
+}
